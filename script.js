f269c8b7917b110873b4db0eca5ec4e8206afe79
Azad Ali <azadsaifi70149@gmail.com>
2025-12-04 19:24:30 +0530

    changes

---
+++ script.js
@@ -0,0 +1,17 @@
+const imgs = Array.from(document.querySelectorAll('#carousel img'));
+const dots = Array.from(document.querySelectorAll('.dot'));
+let index = 0;
+
+function show(i) {
+  imgs.forEach((img, idx) => img.classList.toggle('center', idx === i));
+  dots.forEach((dot, idx) => dot.classList.toggle('active', idx === i));
+  index = i;
+}
+
+dots.forEach(dot => dot.addEventListener('click', () =>
+  show(Number(dot.dataset.i))
+));
+
+setInterval(() => show((index + 1) % imgs.length), 4000);
+
+show(0);
